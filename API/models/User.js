d1ef5be8f331c5687d764a44baf47562dc8b188e
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-07-25 22:12:13 +0530

    user models

--- API/models/User.js
+++ API/models/User.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
-const userSchema=new mongoose.Schema({
-    name:{type:String,required:true},
-    email:{type:String,required:true},
-    password:{type:String,required:true},
-    phone:{type:String,required:true},
-    createdAt:{date:String,default:Date.now()},
-})
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  phone: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
 
-export const User=mongoose.model("User",userSchema)
+export const User = mongoose.model("User", userSchema);
