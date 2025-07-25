e1131d967c8b1bfac572f1185522c0aed220e90a
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-07-25 22:22:56 +0530

    data destructing

--- API/controllers/user.js
+++ API/controllers/user.js
@@ -1,19 +1,15 @@
 import { User } from "../models/User.js";
 
-export const register=async(req,res)=>{
-    const {name,eamil,password}=req.body;
-    try{
-
-        let user=await User.create({
-            name,
-            email,
-            password,
-        });
-        res.json({message:"User registered successfully",success:true});
-
-    }catch(error){
-
-        res.json({message:error.message})
-
-    }
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    let user = await User.create({
+      name,
+      email,
+      password,
+    });
+    res.json({ message: "User registered successfully", success: true });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
 };
