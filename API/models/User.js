4239185edb1c2603930824ef246cf3df7a874744
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-07-25 22:23:35 +0530

    createdAt

--- API/models/User.js
+++ API/models/User.js
@@ -4,7 +4,6 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  phone: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
 });
 
