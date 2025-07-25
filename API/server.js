1853e055dfb4cab28ad88c55c7ae73ea4b14c46f
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-07-25 22:10:31 +0530

    server .js

--- API/server.js
+++ API/server.js
@@ -2,12 +2,26 @@ import express from "express";
 
 import mongoose from "mongoose";
 
+import userRouter from "./Routes/user.js";
+
 const app = express();
 
+//home testing route
+app.get("/", (req, res) => {
+  res.json({message:"this is home route"})
+});
+
+//user router
+
+app.use("/api/user", userRouter)
+
 mongoose
-  .connect("mongodb+srv://url to mongodb", {
-    dbname: "MERN-Ecommerce",
-  })
+  .connect(
+    "mongodb+srv://mongodb url atlas",
+    {
+      dbname: "MERN-Ecommerce",
+    }
+  )
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
