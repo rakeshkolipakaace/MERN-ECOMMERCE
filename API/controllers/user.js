f76b7fc26dd1ca1855bd2ca5df8fbc3d0ebb97de
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-07-25 22:33:26 +0530

    fields verify

--- API/controllers/user.js
+++ API/controllers/user.js
@@ -1,12 +1,24 @@
 import { User } from "../models/User.js";
 
+import bcrypt from "bcryptjs";
+
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
   try {
-    let user = await User.create({
+    if (!name || !email || !password) {
+      throw new Error("Please provide all the fields");
+    }
+    let user = await User.findOne({ email });
+    if (user) {
+      throw new Error("User already exists");
+    }
+
+    const hashpass = await bcrypt.hash(password, 10);
+
+    user = await User.create({
       name,
       email,
-      password,
+      password: hashpass,
     });
     res.json({ message: "User registered successfully", success: true });
   } catch (error) {
--- API/server.js
+++ API/server.js
@@ -2,26 +2,27 @@ import express from "express";
 
 import mongoose from "mongoose";
 
+import bodyParser from "express";
+
 import userRouter from "./Routes/user.js";
 
 const app = express();
 
+app.use(bodyParser.json());
+
 //home testing route
 app.get("/", (req, res) => {
-  res.json({message:"this is home route"})
+  res.json({ message: "this is home route" });
 });
 
 //user router
 
-app.use("/api/user", userRouter)
+app.use("/api/user", userRouter);
 
 mongoose
-  .connect(
-    "mongodb+srv://mongodb url atlas",
-    {
-      dbname: "MERN-Ecommerce",
-    }
-  )
+  .connect("mongodb+srv://monngodb url", {
+    dbname: "MERN-Ecommerce",
+  })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
