12aeef277252da128f3ea8155b7d77d44b0b9d16
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-07-25 22:48:42 +0530

    user.js

--- API/Routes/user.js
+++ API/Routes/user.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { register } from "../controllers/user.js";
+import { login, register } from "../controllers/user.js";
 
 const router=express.Router();
 
 //register user
 
 router.post("/register",register)
+
+//login user
+
+router.post("/login",login)
 
 export default router;
